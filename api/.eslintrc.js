b004aa5f298bfd7ed393e36647a2eb0eedcce974
bocovo <bocovojuste@gmail.com>
2024-08-04 01:47:39 +0100

    chore(api): update eslint configuration

--- api/.eslintrc.js
+++ api/.eslintrc.js
@@ -9,6 +9,7 @@ module.exports = {
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
+    'plugin:import/recommended',
   ],
   root: true,
   env: {
@@ -21,5 +22,26 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          'parent',
+          'sibling',
+          'index',
+          'object',
+          'type',
+        ],
+        'newlines-between': 'always',
+      },
+    ],
   },
+  settings: {
+    "import/resolver": {
+      typescript: true,
+    }
+  }
 };
