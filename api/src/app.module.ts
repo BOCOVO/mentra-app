81314bdec2e32b023524d75afd84a18eecde1628
bocovo <bocovojuste@gmail.com>
2024-08-05 16:52:56 +0100

    chore(api): setup config module

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,14 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import configurations from './config/configurations';
+import { mikroOrmModuleConfig } from './config/mikro-orm.config';
 
 @Module({
+  imports: [
     ConfigModule.forRoot({
       load: [configurations],
     }),
+    MikroOrmModule.forRootAsync(mikroOrmModuleConfig),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
