3b20f4476b8d8c48060d1e1e277f3d73d88b3582
bocovo <bocovojuste@gmail.com>
2024-08-08 23:17:08 +0100

    chore(api): setup graphql server

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MikroOrmModule } from '@mikro-orm/nestjs';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -17,6 +19,11 @@ import { UsersModule } from './modules/users/users.module';
     MikroOrmModule.forRootAsync(mikroOrmModuleConfig),
     AuthModule,
     UsersModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: process.env.GRAPHQL_PLAYGROUND === 'true',
+      autoSchemaFile: true,
+    }),
   ],
   controllers: [AppController],
   providers: [AppService],
