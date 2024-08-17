2c3aacd782d2363de5ee69cb8af56ba1e4be4f05
bocovo <bocovojuste@gmail.com>
2024-08-17 10:10:04 +0100

    chore(mobile): add theme

--- mobile-app/tamagui.config.ts
+++ mobile-app/tamagui.config.ts
@@ -1,7 +1,9 @@
 import { config as configBase } from "@tamagui/config/v3";
 import { createFont, createTamagui, createTokens } from "tamagui";
 
-const { tokens: baseTokens, fonts, ...restBaseConfig } = configBase;
+import * as themes from "./themes";
+
+const { tokens: baseTokens, fonts, themes: _, ...restBaseConfig } = configBase;
 
 const typos = createFont({
   family: "Inter",
@@ -64,55 +66,78 @@ const typos = createFont({
   },
 });
 
+const colors = {
+  "dark.25": "#7A7E80",
+  "dark.50": "#212325",
+  "dark.75": "#161719",
+  "dark.100": "#0D0E0F",
+
+  "light.20": "#91919F",
+  "light.40": "#E3E5E5",
+  "light.60": "#F1F1FA",
+  "light.80": "#FCFCFC",
+  "light.100": "#FFFFFF",
+
+  "violet.20": "#EEE5FF",
+  "violet.40": "#D3BDFF",
+  "violet.60": "#B18AFF",
+  "violet.80": "#8F57FF",
+  "violet.100": "#7F3DFF",
+
+  "red.20": "#FDD5D7",
+  "red.40": "#FDA2A9",
+  "red.60": "#FD6F7A",
+  "red.80": "#FD5662",
+  "red.100": "#FD3C4A",
+
+  "green.20": "#CFFAEA",
+  "green.40": "#93EACA",
+  "green.60": "#65D1AA",
+  "green.80": "#2AB784",
+  "green.100": "#00A86B",
+
+  "yellow.20": "#FCEED4",
+  "yellow.40": "#FCDDA1",
+  "yellow.60": "#FCCC6F",
+  "yellow.80": "#FCBB3C",
+  "yellow.100": "#FCAC12",
+
+  "blue.20": "#BDDCFF",
+  "blue.40": "#8AC0FF",
+  "blue.60": "#57A5FF",
+  "blue.80": "#248AFF",
+  "blue.100": "#0077FF",
+};
+
 // @ts-ignore
 const tokens = createTokens({
   ...baseTokens,
-  color: {
-    "dark.25": "#7A7E80",
-    "dark.50": "#212325",
-    "dark.75": "#161719",
-    "dark.100": "#0D0E0F",
-
-    "light.20": "#91919F",
-    "light.40": "#E3E5E5",
-    "light.60": "#F1F1FA",
-    "light.80": "#FCFCFC",
-    "light.100": "#FFFFFF",
-
-    "violet.20": "#EEE5FF",
-    "violet.40": "#D3BDFF",
-    "violet.60": "#B18AFF",
-    "violet.80": "#8F57FF",
-    "violet.100": "#7F3DFF",
-
-    "red.20": "#FDD5D7",
-    "red.40": "#FDA2A9",
-    "red.60": "#FD6F7A",
-    "red.80": "#FD5662",
-    "red.100": "#FD3C4A",
-
-    "green.20": "#CFFAEA",
-    "green.40": "#93EACA",
-    "green.60": "#65D1AA",
-    "green.80": "#2AB784",
-    "green.100": "#00A86B",
-
-    "yellow.20": "#FCEED4",
-    "yellow.40": "#FCDDA1",
-    "yellow.60": "#FCCC6F",
-    "yellow.80": "#FCBB3C",
-    "yellow.100": "#FCAC12",
-
-    "blue.20": "#BDDCFF",
-    "blue.40": "#8AC0FF",
-    "blue.60": "#57A5FF",
-    "blue.80": "#248AFF",
-    "blue.100": "#0077FF",
-  },
+  color: colors,
 });
 
 export const config = createTamagui({
   tokens,
+  themes: {
+    light: themes.light,
+    primary_btn: {
+      background: colors["violet.100"],
+      color: "#ffffff",
+      backgroundPress: colors["violet.80"],
+      borderColor: "hsla(261, 100%, 84%, 1)",
+      borderColorFocus: "hsla(261, 100%, 84%, 1)",
+      borderColorHover: "hsla(261, 100%, 88%, 1)",
+      borderColorPress: "hsla(261, 100%, 80%, 1)",
+    },
+    secondary_btn: {
+      background: colors["violet.20"],
+      color: colors["violet.100"],
+      backgroundPress: colors["violet.40"],
+      borderColor: "hsla(261, 100%, 84%, 1)",
+      borderColorFocus: "hsla(261, 100%, 84%, 1)",
+      borderColorHover: "hsla(261, 100%, 88%, 1)",
+      borderColorPress: "hsla(261, 100%, 80%, 1)",
+    },
+  },
   fonts: {
     ...fonts,
     // @ts-ignore
