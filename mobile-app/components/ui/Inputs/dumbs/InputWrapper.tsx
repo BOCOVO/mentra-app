0fa66d1fbe2427c7c80e410999a60cdef527f6bb
bocovo <bocovojuste@gmail.com>
2024-09-08 00:00:33 +0100

    chore(mobile): improve input component

--- mobile-app/components/ui/Inputs/dumbs/InputWrapper.tsx
+++ mobile-app/components/ui/Inputs/dumbs/InputWrapper.tsx
@@ -1,4 +1,5 @@
 import { PropsWithChildren } from "react";
+import { useTranslation } from "react-i18next";
 import { SizableText, YStack } from "tamagui";
 
 export interface InputWrapperProps extends PropsWithChildren {
@@ -7,13 +8,15 @@ export interface InputWrapperProps extends PropsWithChildren {
 }
 
 const InputWrapper = ({ children, error, help }: InputWrapperProps) => {
+  const { t } = useTranslation();
+
   return (
     <YStack rowGap="$2">
       {children}
 
       {error || help ? (
         <SizableText size="$tiny" color={error ? "red" : "gray"}>
-          {error || help}
+          {error ? t(error) : help}
         </SizableText>
       ) : null}
     </YStack>
--- mobile-app/components/ui/Inputs/dumbs/TextInput.tsx
+++ mobile-app/components/ui/Inputs/dumbs/TextInput.tsx
@@ -1,9 +1,9 @@
 import { Input, InputProps } from "tamagui";
 import InputWrapper, { InputWrapperProps } from "./InputWrapper";
 
-type TextInputProps = InputProps & InputWrapperProps & {};
+export type TextInputProps = InputProps & InputWrapperProps & {};
 
-const TextInput = ({ error, help }: TextInputProps) => {
+const TextInput = ({ error, help , ...props}: TextInputProps) => {
   return (
     <InputWrapper error={error} help={help}>
       <Input
@@ -11,13 +11,14 @@ const TextInput = ({ error, help }: TextInputProps) => {
         cursorColor="#7F3DFF"
         selectionColor="$violet.60"
         placeholderTextColor="$light.20"
-        focusStyle={{ borderWidth: 2, color: "$light.60" }}
+        focusStyle={{ borderWidth: 2, color: "$light.20" }}
         borderRadius={16}
         paddingVertical={8}
         paddingHorizontal={16}
         borderWidth={1}
         borderColor={error ? "$red.100" : "$light.60"}
         size="$4"
+        {...props}
       />
     </InputWrapper>
   );
