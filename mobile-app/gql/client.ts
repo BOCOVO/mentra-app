112e4ca1134c1150c8d6d7a35f84e9c6b0e8ec31
bocovo <bocovojuste@gmail.com>
2024-09-08 00:01:02 +0100

    chore(mobile): remove console.log

--- mobile-app/gql/client.ts
+++ mobile-app/gql/client.ts
@@ -1,7 +1,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 import { API_URL } from "constants/envs";
 
-console.log(API_URL)
 const apolloClient = new ApolloClient({
   uri: API_URL,
   cache: new InMemoryCache(),
